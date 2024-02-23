55dfae033c5a4737f464616aab4b94803961b3e5
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-02-23 11:21:29 -0800

    Added extended aspect ratio to use in image-pan

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      aspectRatio: {
+        card: "10 / 16",
+      },
     },
   },
   plugins: [],
