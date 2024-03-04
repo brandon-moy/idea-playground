7309b4a28247c00015a8ebb9f37145c3128575de
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-03-03 23:32:37 -0800

    Added styling to x to have border same color as bg and 2 as dark color

--- components/spinners/nikke.tsx
+++ components/spinners/nikke.tsx
@@ -6,23 +6,23 @@ export default function NikkeLoader() {
       <div className="flex w-full h-full flex-wrap">
         <div className="relative basis-1/2">
           <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[4px] bg-gray-300 -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black"></div>
+        </div>
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-600 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[4px] bg-gray-600 -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black"></div>
+        </div>
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-600 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[4px] bg-gray-600 -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black"></div>
         </div>
         <div className="relative basis-1/2">
           <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-        </div>
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-        </div>
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
-          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[4px] bg-gray-300 -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black"></div>
         </div>
       </div>
       {/* LOADING... container */}
-      <div className="flex text-xs font-st-mono text-gray-300">
+      <div className="flex text-xs font-st-mono text-gray-300 tracking-tighter">
         <p className="animate-letter-jump m-0">L</p>
         <p className="animate-letter-jump animation-delay-250 m-0">O</p>
         <p className="animate-letter-jump animation-delay-500 m-0">A</p>
