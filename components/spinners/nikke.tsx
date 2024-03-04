0af75e229433b65fea1807d085bf4866b2356724
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-03-03 23:21:49 -0800

    Created bases for X mark in loading animation

--- components/spinners/nikke.tsx
+++ components/spinners/nikke.tsx
@@ -1,16 +1,28 @@
 export default function NikkeLoader() {
   return (
     // main container
-    <div>
+    <div className="p-10 w-[125px] h-[125px]">
       {/* x  container */}
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex w-full h-full flex-wrap">
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+        </div>
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+        </div>
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+        </div>
+        <div className="relative basis-1/2">
+          <div className="w-[15px] h-[2.5px] bg-gray-300 rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+          <div className="w-[15px] h-[2.5px] bg-gray-300 -rotate-45 absolute translate-x-1 translate-y-[0.6rem]"></div>
+        </div>
       </div>
       {/* LOADING... container */}
-      <div className="flex font-st-mono text-gray-300">
+      <div className="flex text-xs font-st-mono text-gray-300">
         <p className="animate-letter-jump m-0">L</p>
         <p className="animate-letter-jump animation-delay-250 m-0">O</p>
         <p className="animate-letter-jump animation-delay-500 m-0">A</p>
