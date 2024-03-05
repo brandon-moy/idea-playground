646317499dcd3e759d215c6f858170fb99cb608f
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-03-05 15:19:24 -0800

    Adjusted sizing and created border card for loading spinner including name of where spinner is from

--- app/loading-spinners/page.tsx
+++ app/loading-spinners/page.tsx
@@ -2,7 +2,8 @@ import NikkeLoader from "@/components/spinners/nikke";
 
 function LoadingSpinners() {
   return (
-    <div>
+    <div className="w-[125px] h-[150px] border border-gray-600 rounded-xl bg-black">
+      <p className="text-xs p-2 font-st-mono">NIKKE: Goddess of Victory</p>
       <NikkeLoader />
     </div>
   );
--- components/spinners/nikke.tsx
+++ components/spinners/nikke.tsx
@@ -1,28 +1,30 @@
 export default function NikkeLoader() {
   return (
     // main container
-    <div className="p-10 w-[125px] h-[125px]">
+    <>
       {/* x  container */}
-      <div className="flex w-full h-full flex-wrap mb-2 scale-90 animate-[nikkeLoad_4s_linear_infinite]">
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeLightX_4s_ease_infinite]"></div>
-          <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeLightX_4s_ease_infinite]"></div>
-        </div>
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeDarkX_4s_ease_infinite]"></div>
-          <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeDarkX_4s_ease_infinite]"></div>
-        </div>
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeDarkX_4s_ease_infinite]"></div>
-          <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeDarkX_4s_ease_infinite]"></div>
-        </div>
-        <div className="relative basis-1/2">
-          <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeLightX_4s_ease_infinite]"></div>
-          <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeLightX_4s_ease_infinite]"></div>
+      <div className="w-full flex justify-center">
+        <div className="w-[50px] h-[50px] flex flex-wrap mb-2 scale-90 animate-[nikkeLoad_4s_linear_infinite]">
+          <div className="relative basis-1/2">
+            <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeLightX_4s_ease_infinite]"></div>
+            <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeLightX_4s_ease_infinite]"></div>
+          </div>
+          <div className="relative basis-1/2">
+            <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeDarkX_4s_ease_infinite]"></div>
+            <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeDarkX_4s_ease_infinite]"></div>
+          </div>
+          <div className="relative basis-1/2">
+            <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeDarkX_4s_ease_infinite]"></div>
+            <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeDarkX_4s_ease_infinite]"></div>
+          </div>
+          <div className="relative basis-1/2">
+            <div className="w-[15px] h-[2.5px] rotate-45 absolute translate-x-1 translate-y-[0.6rem] animate-[nikkeLightX_4s_ease_infinite]"></div>
+            <div className="w-[15px] h-[4px] -rotate-45 absolute translate-x-[0.23rem] translate-y-[0.55rem] border-t-[1.5px] border-black animate-[nikkeLightX_4s_ease_infinite]"></div>
+          </div>
         </div>
       </div>
       {/* LOADING... container */}
-      <div className="flex text-xs font-st-mono text-gray-300 tracking-tighter">
+      <div className="flex text-xs font-st-mono text-gray-300 tracking-tighter px-10">
         <p className="animate-letter-jump m-0">L</p>
         <p className="animate-letter-jump animation-delay-250 m-0">O</p>
         <p className="animate-letter-jump animation-delay-500 m-0">A</p>
@@ -34,6 +36,6 @@ export default function NikkeLoader() {
         <p className="animate-letter-jump animation-delay-2000 m-0">.</p>
         <p className="animate-letter-jump animation-delay-2250 m-0">.</p>
       </div>
-    </div>
+    </>
   );
 }
